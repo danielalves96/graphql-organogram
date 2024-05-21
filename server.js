e64e93148be802f6698dbad4c9b3dd0ebf28c9af
Daniel Alves <daniel.xcoders@gmail.com>
2024-05-21 20:56:21 -0300

    change to express

--- server.js
+++ server.js
@@ -1,40 +1,27 @@
-// server.js
-const Fastify = require('fastify');
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const fastifyExpress = require('fastify-express');
 const { typeDefs, resolvers } = require('./schema');
-const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 async function startServer() {
-  const app = Fastify();
-
-  await app.register(fastifyExpress);
+  const app = express();
 
   const schema = makeExecutableSchema({ typeDefs, resolvers: resolvers(prisma) });
   const apolloServer = new ApolloServer({ schema });
 
   await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-  const expressApp = express();
-  apolloServer.applyMiddleware({ app: expressApp });
-
-  app.use(expressApp);
-
-  app.get('/', async (request, reply) => {
-    reply.send({ hello: 'world' });
+  app.get('/', (req, res) => {
+    res.send({ hello: 'world' });
   });
 
   const port = process.env.PORT || 4000;
-  app.listen({ port }, (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`🚀 Server ready at ${address}/graphql`);
+  app.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`);
   });
 }
 
